cd2759cbf86d72dab2f10f8f5974cd3333cd8077
shubham chandra <shubhamchandra317@gmail.com>
2020-08-26 22:30:19 +0530

    use ConstraintLayout

--- app/src/main/java/com/shubham/criminalintent/Fragment/CrimeListFragment.kt
+++ app/src/main/java/com/shubham/criminalintent/Fragment/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -49,6 +50,7 @@ class CrimeListFragment:Fragment() {
         private lateinit var crime:Crime
         private val titleTextView:TextView = itemView.findViewById(R.id.crime_title)
         private val dateTextView:TextView = itemView.findViewById(R.id.crime_date)
+        private val solvedImageView:ImageView = itemView.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -57,6 +59,13 @@ class CrimeListFragment:Fragment() {
             this.crime = crime
             titleTextView.text = crime.title
             dateTextView.text = crime.date.toString()
+
+
+            solvedImageView.visibility = if (crime.isSolved){
+                View.VISIBLE
+            }else{
+                View.GONE
+            }
 
         }
 
